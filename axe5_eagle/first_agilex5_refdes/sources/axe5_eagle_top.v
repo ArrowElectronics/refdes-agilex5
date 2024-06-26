a7c83739f6c199ae7c6d5a37a9afdec48220859e
Steven Kravatsky <skravatsky@arrow.com>
2024-06-26 15:14:28 -0400

    Added Reset Release IP for post configuration reset deassertion
    Date:      Wed Jun 26 15:03:26 2024 -0400
    
    Changes to be committed:
        modified:   axe5_eagle/first_agilex5_refdes/axe5_eagle_top.qsf
        new file:   axe5_eagle/first_agilex5_refdes/rrip.ip
        modified:   axe5_eagle/first_agilex5_refdes/sources/axe5_eagle_top.v
    
    Signed-off-by: Steven Kravatsky <skravatsky@arrow.com>

--- axe5_eagle/first_agilex5_refdes/sources/axe5_eagle_top.v
+++ axe5_eagle/first_agilex5_refdes/sources/axe5_eagle_top.v
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT-0 
 //
 // This simple project uses a counter running at 25MHz clock to toggle and LED
-// FPGA_PB2 and FPGA_PB3 are sed to drive 2 other LEDs.
+// FPGA_PB2 and FPGA_PB3 are used to drive 2 other LEDs.
 // FPGA_PB2 pressed -> RGB_LED1.Green is on
 // FPGA_PB3 pressed -> RGB_LED1.Blue is on
 // The counter timeout marched the colors of RGB_LED0 R -> G -> B -> R ...
@@ -12,19 +12,23 @@
 
 module axe5_eagle_top (
 
-   inout        	LED0R, LED0G, LED0B,
+   output        	LED0R, LED0G, LED0B,
 
    output        	LED1R, LED1G, LED1B,
 
-   input [1:0]   	FPGA_PB,		// Labeled FPGA_PB3:FPGA_PB3 on the PCB
+   input [1:0]   	FPGA_PB,		// Labeled FPGA_PB3:FPGA_PB2 on the PCB
 
-   input            FPGA_25M_CLK,	// 25MHz single-ended clock input
    input         	REFCLK_3B0,		// 25MHz differential clock input
    input         	FPGA_RST_n		// active-low reset from RST button
    
 );
 
-   reg [2:0] rgb_led0;
+
+   wire      ninit_done;
+	wire      combo_reset;
+	reg       rs1, reset_synched;
+	
+   reg [2:0] rgb_led0;          // 3-bit shift register for driving RGB_LED0
 
    assign LED0R = rgb_led0[2];
    assign LED0G = rgb_led0[1];
@@ -37,6 +41,24 @@ module axe5_eagle_top (
    assign LED1B = rgb_led1[0];
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Instantiate the Agilex Reset Release module
+rrip reset_release (
+		.ninit_done (ninit_done)  //  output,  width = 1, ninit_done.reset
+	);
+
+
+// Combine ninit_done with FPGA_RST_n
+assign combo_reset = !FPGA_RST_n | ninit_done;
+
+// Synchronize the Reset action to the clock.
+always @ (posedge REFCLK_3B0) begin
+	rs1           <= combo_reset;
+	reset_synched <= rs1;
+end
+
+
+
 // Add counter, push-button, and LED logic
 
 	reg [24:0] counter;
@@ -44,8 +66,8 @@ module axe5_eagle_top (
 // The Counter counts up from 0x0000000 to 0x1000000.
 // Every time it reaches the terminal count, it resets itself to 0x0000000, and
 // selects the next LED0 color to be illuminated
-always @ (posedge REFCLK_3B0 or negedge FPGA_RST_n) begin
-	if (!FPGA_RST_n)
+always @ (posedge REFCLK_3B0 or posedge reset_synched) begin
+	if (reset_synched)
 		counter <= 25'h0;
 	else if (counter[24])
 		counter <= 25'h0;
@@ -54,9 +76,9 @@ always @ (posedge REFCLK_3B0 or negedge FPGA_RST_n) begin
 end
 
 // march the colors in rgb_led0 every time the counter times out
-always @ (posedge REFCLK_3B0 or negedge FPGA_RST_n) 
+always @ (posedge REFCLK_3B0 or posedge reset_synched) 
 begin
-	if (!FPGA_RST_n)
+	if (reset_synched)
 		rgb_led0 <= 3'h7;		// all LEDs off
 	else if (counter[24]) begin
 		if ((rgb_led0 == 3'h7) || (rgb_led0 == 3'b110))
@@ -69,8 +91,8 @@ begin
 end
 
 // push-buttons (no debouncing)
-assign rgb_led1[2] = 1'b1;
-assign rgb_led1[1] = FPGA_PB[0];
-assign rgb_led1[0] = FPGA_PB[1];
+assign rgb_led1[2] = 1'b1;			// RGB_LED1.Red not used
+assign rgb_led1[1] = FPGA_PB[0];	// RGB_LED1.Green
+assign rgb_led1[0] = FPGA_PB[1];	// RGB_LED1.Blue
 
 endmodule
